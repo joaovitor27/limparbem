07f541087957638909d44b1e2ead1aca41cac689
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2022-04-05 21:31:43 -0300

    registrando models no admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,29 @@
+
 from django.contrib import admin
 
+from .models import Services, Office, Employees, Attendance
 # Register your models here.
+class AdminServices(admin.ModelAdmin):
+    list_display = [
+        'name', 'value'
+    ]
+
+class AdminOffice(admin.ModelAdmin):
+    list_display = [
+        'office'
+    ]
+
+class AdminEmployees(admin.ModelAdmin):
+    list_display = [
+        'users'
+    ]
+
+class AdminAttendance(admin.ModelAdmin):
+    list_display = [
+        'services', 'users', 'discount', 'total_value', 'date_attendance', 'date_service', 'status_attendance', 'client_name', 'client_phone', 'client_address'
+    ]
+
+admin.site.register(Services, AdminServices)
+admin.site.register(Office, AdminOffice)
+admin.site.register(Employees, AdminEmployees)
+admin.site.register(Attendance, AdminAttendance)
